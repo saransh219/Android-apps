62d3fda4732d3d786537649cf95a5019826db446
Saransh Jain <saranshjain22@gmail.com>
2018-04-26 21:56:42 +0530

    Coffee App-1

--- JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -21,7 +21,7 @@ import java.text.NumberFormat;
  */
 public class MainActivity extends AppCompatActivity {
 
-    int numberOfCoffee=2;
+    int numberOfCoffee=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,15 +32,15 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-       // int numberOfCoffees=2;
-        //display(numberOfCoffee);
-        displayPrice(numberOfCoffee*10);
+        int price= numberOfCoffee*10;
+        String priceMessage="Amount Due $ " + price;
+        priceMessage=priceMessage+ "\nThank you";
+        displayMessage(priceMessage);
     }
     /**
      * This method is called when the plus button is clicked.
      */
     public void increment(View view) {
-        //int numberOfCoffee=2;
         numberOfCoffee=numberOfCoffee+1;
         display(numberOfCoffee);
     }
@@ -48,7 +48,6 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the subtraction button is clicked.
      */
     public void decrement(View view) {
-        //int numberOfCoffee=2;
         numberOfCoffee=numberOfCoffee-1;
         display(numberOfCoffee);
     }
@@ -65,5 +64,12 @@ public class MainActivity extends AppCompatActivity {
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
+    }
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
     }
 }
